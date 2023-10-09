82ffe27553851df233d7c77fc11a87a009fa48ca
meiirbek.islamov <mei12@pitt.edu>
2023-10-08 21:09:09 -0400

    remove unneccesarycomments and white spaces

--- conv.py
+++ conv.py
@@ -1,13 +1,9 @@
-# Do not import any additional 3rd party external libraries as they will not
-# be available to AutoLab and are not needed (or allowed)
-
 import numpy as np
 from resampling import *
 
 class Conv1d_stride1():
     def __init__(self, in_channels, out_channels, kernel_size,
                  weight_init_fn=None, bias_init_fn=None):
-        # Do not modify this method
         self.in_channels = in_channels
         self.out_channels = out_channels
         self.kernel_size = kernel_size
@@ -83,8 +79,6 @@ class Conv1d_stride1():
 class Conv1d():
     def __init__(self, in_channels, out_channels, kernel_size, stride,
                 weight_init_fn=None, bias_init_fn=None):
-        # Do not modify the variable names
-
         self.in_channels = in_channels
         self.out_channels = out_channels
         self.kernel_size = kernel_size
@@ -136,8 +130,6 @@ class Conv1d():
 class Conv2d_stride1():
     def __init__(self, in_channels, out_channels,
                  kernel_size, weight_init_fn=None, bias_init_fn=None):
-
-        # Do not modify this method
 
         self.in_channels = in_channels
         self.out_channels = out_channels
@@ -269,7 +261,6 @@ class Conv2d():
 class ConvTranspose1d():
     def __init__(self, in_channels, out_channels, kernel_size, upsampling_factor,
                 weight_init_fn=None, bias_init_fn=None):
-        # Do not modify this method
 
         self.in_channels = in_channels
         self.out_channels = out_channels
